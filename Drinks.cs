2c322509ae297007c865350b7dc0ecab4e46d55d
Tyler <claypooltyler00@gmail.com>
2020-03-15 14:43:04 -0500

    Added Drinks class and drinks options

---
+++ Drinks.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace Project0_2
+{
+    public class Drinks : Product, IAddChoices
+    {
+        public Drinks()
+        {
+            Name = "Drinks";
+        }
+
+        public Dictionary<string,double> ProductChoices()
+        {
+            Choices.Add("Pepsi", 1.99);
+            Choices.Add("Sprite", 1.99);
+            Choices.Add("Dr. Pepper", 1.99);
+            Choices.Add("Root Beer", 2.99);
+
+            return Choices;
+        }
+    }
+}
