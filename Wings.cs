256dd8e68f8f188cb78c6c8ee837796bd36399c6
Tyler <claypooltyler00@gmail.com>
2020-03-15 14:01:18 -0500

    Added Wings class and wings options

---
+++ Wings.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Project0_2
+{
+    public class Wings : Product, IAddChoices
+    {
+        public Wings()
+        {
+            Name = "Wing";
+        }
+
+        public Dictionary<string,double> ProductChoices()
+        {
+            Choices.Add("Tradional", 7.39);
+            Choices.Add("Breaded Bone-Out", 7.5);
+            Choices.Add("Dip", .79);
+
+            return Choices;
+        }
+    }
+}
